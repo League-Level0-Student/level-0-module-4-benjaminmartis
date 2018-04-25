16aa7afdbe44277fddb2835e57c491cdda2b5f67
Martis.Ben <Martis.Ben@SEMS-259-36.SEMS259.LAB>
2018-04-25 14:58:30 -0700

    happy pet

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 
 public class HappyPet {
 	
@@ -8,25 +9,32 @@ public class HappyPet {
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 		String pet = JOptionPane.showInputDialog("What pet would you like?");
-		}
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-
-	private String pet;
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task1 = JOptionPane.showOptionDialog(null, "what do you want to do with your"+pet+"?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "feed your bois", "walk your bois", "cuddle your bois", "wash your bois" ,"clean up poops"}, null);
+			int task = JOptionPane.showOptionDialog(null, "What would you like to do with your " +pet+ "?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Snuggle with "+pet+"?", "Poop with your "+pet+ "?", "Eat with your " +pet+"?"}, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+              
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
-	}
-
+	
+	
+	 if (task==0) {
+	 JOptionPane.showMessageDialog(null, "Your "+pet+ " really enjoyed that peaceful time with you!*"+pet+ " made cute noise*");
+	 }
+	 if (task==1) {
+		 JOptionPane.showMessageDialog(null, "Your "+pet+" Did not particularly enjoy that incident. *"+pet+" made a not so cute snarling noise*");
+	 }
+	 if (task==2) {
+		 JOptionPane.showMessageDialog(null, "Your "+pet+ "enjoyed your company quite so! *"+pet+" made a relatively mediocre noise*");
+	 }
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+}
+}
