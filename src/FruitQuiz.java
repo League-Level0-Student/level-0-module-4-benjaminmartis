dc02bc58311e95680514e1f5c4ad0083a3679fcf
Martis.Ben <Martis.Ben@SEMS-259-36.SEMS259.LAB>
2018-05-16 14:58:23 -0700

    own adventure

--- src/FruitQuiz.java
+++ src/FruitQuiz.java
@@ -21,9 +21,11 @@ public class FruitQuiz extends KeyAdapter {
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+		System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+		int keyA = 1;
+		int keyB = 2;
+		int keyC = 3;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
--- src/Own_adventure.java
+++ src/Own_adventure.java
@@ -2,13 +2,40 @@ import javax.swing.JOptionPane;
 
 public class Own_adventure {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		JOptionPane.showConfirmDialog(null, "You are /");
-		
-		
-		
-		
+	public static void main(String[] args) {	
+		int grab = JOptionPane.showOptionDialog(null, "You are about to go to the grocery store, what do you do first?", "At home", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Grab phone", "Grab keys", "Grab and put your pants on"}, null);
+		if (grab==0) {
+			JOptionPane.showMessageDialog(null, "Your phone starts growing legs and arms and starts punching you until you go unconscious into a coma, then later in the hospital you died... :C");
+		}
+		if (grab==1) {
+			JOptionPane.showMessageDialog(null, "You git into your car and after driving for about 3 miles you realize that you forgot your phone and your pants!");
+			int drive = JOptionPane.showConfirmDialog(null, "Do you turn around and get your forgotten items?");
+			if(drive==0) {
+				JOptionPane.showMessageDialog(null, "You turned to fast in embarassment so you flipped over and crashed. AKA you died fam");
+			}
+			if(drive==1) {
+				JOptionPane.showMessageDialog(null, "After drving around in the parking lot of your local grocery store for about a total of 30 minutes you finaly get a parking spot.");
+				int plotdo = JOptionPane.showOptionDialog(null, "once you go out of your car many eyes turned to your unclothed buttox.","At parking lot", 0, JOptionPane.INFORMATION_MESSAGE, null,
+						new String[] { "Jump back into car after the ambarresing incident.", "Run to the nearest open pair of clothes"}, null);
+			}
+		}
+		if(grab==2) {
+			JOptionPane.showMessageDialog(null, "You made the sensible thing and put some clothes on");
+			int afterpants = JOptionPane.showOptionDialog(null, "Now do you get your phone or get your keys?","At home still", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Go and get your phone", "Go and get your keys"}, null);
+			if(afterpants == 0) {
+				JOptionPane.showMessageDialog(null, "Your phone starts growing legs and arms and starts punching you until you go unconscious");
+			}
+			if(afterpants == 1) {
+				JOptionPane.showMessageDialog(null, "You grabbed your keys and got into the car");
+				JOptionPane.showMessageDialog(null, "You git into your car and after driving for about 3 miles you realize that you forgot your phone and your pants!");
+				int driveafterpants = JOptionPane.showConfirmDialog(null, "Do you turn around and get your forgotten phone? Plus your special someone might have called or got home before you did and started looking through your phone.");
+				if (driveafterpants == 0) {
+					JOptionPane.showMessageDialog(null, "After your sudden realization you tried to turn around so fast that you flipped over and crashed. AKA you died fam");
+				}
+			}
+		}
+		 
+		 }
 	}
-
-}
